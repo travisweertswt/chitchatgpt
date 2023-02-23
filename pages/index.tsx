aa7a52692cc02506965c991e9128a05b5b584ca2
Travis Weerts <83998159+travisweertswt@users.noreply.github.com>
2023-02-23 10:09:36 +0800

    fix: added bad vibes detection

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,11 +3,19 @@ import { useState } from "react";
 import { useEffect } from "react";
 import { useRouter } from "next/router";
 import { Configuration, OpenAIApi } from "openai";
+import axios from "axios";
+
+// import NaturalLanguageUnderstandingV1 from "ibm-watson/natural-language-understanding/v1";
+// import { IamAuthenticator } from "ibm-watson/auth";
+
+//const NaturalLanguageUnderstandingV1 = require("ibm-watson/natural-language-understanding/v1");
+// const { IamAuthenticator } = require("ibm-watson/auth");
 
 const Home = () => {
   const router = useRouter();
   const [input, setInput] = useState("");
   const [loading, setLoading] = useState(false);
+  const [badVibes, setBadVibes] = useState(false);
   const [yourName, setYourName] = useState("");
   const [output, setOutput] = useState("");
 
@@ -16,9 +24,30 @@ const Home = () => {
   });
   const openai = new OpenAIApi(configuration);
 
-  const getResponse = async (input: string, yourName: string) => {
-    console.log("getting response for: " + input);
+  const getAltOpenAIResponse = async (input: string, yourName: string) => {
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt:
+        "In a super cheeky Gen Z comedy style, write an insanely funny message back about how you know what they are up to, and how they should consider being less negative and that there is enough negativity in the world and maybe they should give negativity a break and have a KitKat instead.",
+      temperature: 0.7,
+      max_tokens: 1055,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+    });
 
+    let outputFormatted = response.data.choices[0].text || "";
+
+    // outputFormatted = outputFormatted.replace("\n", "<br />");
+
+    setOutput(outputFormatted);
+
+    console.log(response.data.choices[0].text);
+    setBadVibes(true);
+    setLoading(false);
+  };
+
+  const getOpenAIResponse = async (input: string, yourName: string) => {
     const response = await openai.createCompletion({
       model: "text-davinci-003",
       prompt:
@@ -44,6 +73,80 @@ const Home = () => {
     setLoading(false);
   };
 
+  const getResponse = async (input: string, yourName: string) => {
+    console.log("getting response for: " + input);
+
+    // const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
+    //   version: "2022-04-07",
+    //   authenticator: new IamAuthenticator({
+    //     apikey: "e5AVUgDFcpVm4bHGharvKxwz96kE6ndUFAmhSR43AmkQ",
+    //   }),
+    //   serviceUrl:
+    //     "https://api.au-syd.natural-language-understanding.watson.cloud.ibm.com/instances/0a56f75e-dbae-4b48-ae8b-24964b65ac89",
+    // });
+
+    const analyzeParams = {
+      text:
+        "" +
+        "to: " +
+        input +
+        " from: " +
+        yourName +
+        " " +
+        "to: " +
+        input +
+        " from: " +
+        yourName +
+        " " +
+        "to: " +
+        input +
+        " from: " +
+        yourName +
+        " " +
+        "to: " +
+        input +
+        " from: " +
+        yourName +
+        " ",
+      features: {
+        sentiment: {},
+      },
+    };
+
+    await axios
+      .post(
+        "https://api.au-syd.natural-language-understanding.watson.cloud.ibm.com/instances/0a56f75e-dbae-4b48-ae8b-24964b65ac89/v1/analyze?version=2022-04-07",
+        analyzeParams,
+        {
+          auth: {
+            username: "apikey",
+            password: "e5AVUgDFcpVm4bHGharvKxwz96kE6ndUFAmhSR43AmkQ",
+          },
+        }
+      )
+      .then(function (response) {
+        console.log("Authenticated");
+        console.log(response);
+        if (response.data.sentiment.document.score < 0) {
+          getAltOpenAIResponse(input, yourName);
+        } else {
+          getOpenAIResponse(input, yourName);
+        }
+      })
+      .catch(function (error) {
+        console.log("Error on Authentication");
+      });
+
+    // naturalLanguageUnderstanding
+    //   .analyze(analyzeParams)
+    //   .then((analysisResults: any) => {
+    //     console.log(JSON.stringify(analysisResults, null, 2));
+    //   })
+    //   .catch((err: any) => {
+    //     console.log("error:", err);
+    //   });
+  };
+
   useEffect(() => {}, [input]);
 
   const handleCopyClick = (e: any) => {
@@ -52,6 +155,7 @@ const Home = () => {
 
   const handleBackClick = (e: any) => {
     setLoading(false);
+    setBadVibes(false);
     setYourName("");
     setInput("");
     setOutput("");
@@ -59,15 +163,18 @@ const Home = () => {
 
   const handleButtonClick = (e: any) => {
     setLoading(true);
+    setBadVibes(false);
     getResponse(input, yourName);
   };
   // input change
 
   const handleInputChange = (e: any) => {
+    setBadVibes(false);
     setInput(e.target.value);
   };
 
   const handleYourNameChange = (e: any) => {
+    setBadVibes(false);
     setYourName(e.target.value);
   };
 
@@ -80,10 +187,16 @@ const Home = () => {
       )}
       {!loading && output && output.length > 0 && (
         <>
-          <h3>NOW... COPY AND SEND THIS TO {input}:</h3>
+          {badVibes && (
+            <h3>HMMM... KINDA GETTING BAD VIBES ABOUT THAT ONE...</h3>
+          )}
+
+          {!badVibes && <h3>NOW... COPY AND SEND THIS TO {input}:</h3>}
+
           <div>{output}</div>
           <div className="doneButtons">
-            <button onClick={handleCopyClick}>Copy</button>
+            {!badVibes && <button onClick={handleCopyClick}>Copy</button>}
+
             <button onClick={handleBackClick}>Try Again</button>
           </div>
         </>
